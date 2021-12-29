e08348a558d948314ace35d69972aea3f86c5fdc
Arnab Sahoo <arnabsahoo10@gmail.com>
2021-12-29 14:53:26 +0530

    added retriveQuestionData func that fetch and store upvote ans count in db also fetch related questions

--- helpers/questionFetcher.js
+++ helpers/questionFetcher.js
@@ -1,17 +1,56 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-
-exports.fetchQuestionsByPageNumber = async (pageNumber, setTaskInTasksQueue) => {
+const { Question } = require("../models/question");
+const crawlerConstants=require("../constants/crawlerConsts");
+const crawlerConsts = require("../constants/crawlerConsts");
+const fetchQuestionsByPageNumber = async (pageNumber, setTaskInTasksQueue) => {
 	try {
 		const response = await axios.get(`https://stackoverflow.com/questions?&page=${pageNumber}`);
 		const $ = cheerio.load(response.data);
-		const links = $("h3>a.question-hyperlink")
+		const links = $(crawlerConsts.FETCH_QUESTION_FROM_QUESTION_PAGE_SELECTOR)
 			.map((i, link) => `https://stackoverflow.com${link.attribs.href}`)
 			.get();
-        console.log("links",links)
 		setTaskInTasksQueue(links);
 	} catch (error) {
 		setTaskInTasksQueue([]);
 		console.log("Error occured in fetchQuestionsByPageNumber ", error);
 	}
 };
+
+exports.retriveQuestionData = async (questionUrl, currentPage, setTaskInTasksQueue) => {
+	const urlData = {
+		url:questionUrl,
+	};
+
+	const { data } = await axios.get(questionUrl);
+	const $ = cheerio.load(data);
+
+	const noans = $(crawlerConsts.NO_ANS_SELECTOR);
+	const upvote = $(crawlerConsts.UPVOTE_COUNT_SELECTOR).text().trim().split(" ")[0].trim();
+
+	if (noans.length > 0) {
+		urlData.noOfAnsweres = 0;
+		urlData.noOfUpvotes = upvote;
+	} else {
+		const noOfAnswares = $(crawlerConsts.NUMBER_OF_ANS_SELECTOR).text().trim();
+		urlData.noOfAnsweres = noOfAnswares;
+		urlData.noOfUpvotes = upvote;
+	}
+	const relatedQuestions = $(crawlerConsts.RELETED_QUESTION_SELECTOR)
+		.map((i, link) => `https://stackoverflow.com${link.attribs.href}`)
+		.get();
+	if (relatedQuestions.length > 0) {
+		setTaskInTasksQueue(relatedQuestions);
+	} else {
+		await fetchQuestionsByPageNumber(currentPage + 1, setTaskInTasksQueue);
+		currentPage += 1;
+	}
+
+	const newQuestion = new Question({
+		...urlData,
+		totalReferenceCount: 1,
+	});
+	await newQuestion.save();
+};
+
+exports.fetchQuestionsByPageNumber = fetchQuestionsByPageNumber;
