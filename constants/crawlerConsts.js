9025f2e5a9456a6215cc888fe18f7acf86c0f449
Arnab Sahoo <arnabsahoo10@gmail.com>
2021-12-29 14:51:44 +0530

    added crawler constants

---
+++ constants/crawlerConsts.js
@@ -0,0 +1,7 @@
+module.exports={
+    FETCH_QUESTION_FROM_QUESTION_PAGE_SELECTOR:"h3>a.question-hyperlink",
+    UPVOTE_COUNT_SELECTOR:"div[itemprop=upvoteCount]",
+    NO_ANS_SELECTOR:"div.no-answers",
+    NUMBER_OF_ANS_SELECTOR:"span[itemprop=answerCount]",
+    RELETED_QUESTION_SELECTOR:"div>a.question-hyperlink"
+}
